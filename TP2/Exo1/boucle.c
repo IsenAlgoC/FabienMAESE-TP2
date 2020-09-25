c74fd3ee19cf99598bbb3cd1e7e979282570b615
Fabien MAESE <fabien.maese@isen.yncrea.fr>
2020-09-25 10:11:25 +0200

    Demander la valeur de n à l’utilisateur, sans check

--- TP2/Exo1/boucle.c
+++ TP2/Exo1/boucle.c
@@ -3,23 +3,30 @@
 
 int main() {
 	
-											// La valeur maximale pour un unsigned short int est 65,535 (car int est codé sur 16 bits)
-											// Donc la valeur maximale de n est 361
-	int n = 361;
-	int k = 0;
-	int sum = 0;
+										// La valeur maximale pour un unsigned short int est 65,535 (car short int est codé sur 16 bits)
+										// Donc la valeur maximale de n est 361
+	short int n = 0;
+	short int k = 0;
+	short int sum = 0;
 
-	while (k <= n) {							// avec une boucle while
-		if (65535 - sum <= k) {
-			printf("\nCapacite de calcul depassee, entrer un entier moins grand\n");
-			break;
-		}
+	//while (k <= n) {							// avec une boucle while
+	//	if (65535 - sum <= k) {
+	//		printf("\nCapacite de calcul depassee, entrer un entier moins grand\n");
+	//		break;
+	//	}
+	//	sum += k;
+	//	k += 1;
+	//}
+	//int A = 65536 - sum;
+	//if (A >= 0) {
+	//	printf("\nLa somme des %d premiers entiers est %d\n", n, sum);
+	//}
+
+	printf("entrez un entier: ");
+	scanf_s("%hu", &n);
+	for (k; k <= n; ++k) {						// Au-delà des valeurs n permises, la somme est négative
 		sum += k;
-		k += 1;
 	}
-	int A = 65536 - sum;
-	if (A >= 0) {
-		printf("\nLa somme des %d premiers entiers est %d\n", n, sum);
-	}
+	printf("\nLa somme des %d premiers entiers est %d\n", n, sum);
 	
 }
