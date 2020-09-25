5105e390cebea90e884493c9c5673ccad50db9a6
Fabien MAESE <fabien.maese@isen.yncrea.fr>
2020-09-25 09:57:00 +0200

    2e point : Trouver la valeur maximum de n sans dépassement de capacité (test dans la boucle while)

--- TP2/Exo1/boucle.c
+++ TP2/Exo1/boucle.c
@@ -3,32 +3,23 @@
 
 int main() {
 	
-	int n = 100;
-	int i = 0;
-	int sum1 = 0;
-
-	for (i; i <= n; ++i) {						// avec une boucle for
-		sum1 += i;
-	}
-	printf("\nLa somme des %d premiers entiers est %d\n", n, sum1);
-
-	int j = 0;
-	int sum2 = 0;
-
-	do {										// avec une boucle do...while
-		sum2 += j;
-		j += 1;
-	} while (j <= n);
-
-	printf("\nLa somme des %d premiers entiers est %d\n", n, sum2);
-
-
+											// La valeur maximale pour un unsigned short int est 65,535 (car int est codé sur 16 bits)
+											// Donc la valeur maximale de n est 361
+	int n = 361;
 	int k = 0;
-	int sum3 = 0;
+	int sum = 0;
 
 	while (k <= n) {							// avec une boucle while
-		sum3 += k;
+		if (65535 - sum <= k) {
+			printf("\nCapacite de calcul depassee, entrer un entier moins grand\n");
+			break;
+		}
+		sum += k;
 		k += 1;
 	}
-	printf("\nLa somme des %d premiers entiers est %d\n", n, sum3);
+	int A = 65536 - sum;
+	if (A >= 0) {
+		printf("\nLa somme des %d premiers entiers est %d\n", n, sum);
+	}
+	
 }
