be7ab90349a6977c1c9a0c1513d861f09b993d3e
Fabien MAESE <fabien.maese@isen.yncrea.fr>
2020-09-25 10:26:24 +0200

    Recommencer tant que n est trop grand

--- TP2/Exo1/boucle.c
+++ TP2/Exo1/boucle.c
@@ -5,9 +5,16 @@ int main() {
 	
 										// La valeur maximale pour un unsigned short int est 65,535 (car short int est codé sur 16 bits)
 										// Donc la valeur maximale de n est 361
-	short int n = 0;
-	short int k = 0;
-	short int sum = 0;
+	unsigned short int n = 0;
+	unsigned short int k = 0;
+	unsigned short int sum = 0;
+
+	//int j = 0;
+	//int sum2 = 0;
+	//do {										// avec une boucle do...while
+	//	sum2 += j;
+	//	j += 1;
+	//} while (j <= n);
 
 	//while (k <= n) {							// avec une boucle while
 	//	if (65535 - sum <= k) {
@@ -22,8 +29,14 @@ int main() {
 	//	printf("\nLa somme des %d premiers entiers est %d\n", n, sum);
 	//}
 
+		
 	printf("entrez un entier: ");
 	scanf_s("%hu", &n);
+	while (n > 361) {											// recommencer la saisie tant que n est trop grand 
+		printf("entrez un autre entier plus petit: ");
+		scanf_s("%hu", &n);
+	}
+
 	for (k; k <= n; ++k) {						// Au-delà des valeurs n permises, la somme est négative
 		sum += k;
 	}
